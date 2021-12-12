d8bfbfdf9228f521cfcc0b3bee940509f04faddb
kazuki827 <xm.kazuki827@gmail.com>
2021-12-12 22:25:25 +0900

    enum

--- object.ts
+++ object.ts
@@ -1,18 +1,30 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  name: 'john',
-  age: 30,
-  // Array
-  hobbies: ['Sport', 'Cooking'],
-  // Tuple(長さ固定の配列)
-  role: [2, 'author']
-};
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'john',
+//   age: 30,
+//   // Array
+//   hobbies: ['Sport', 'Cooking'],
+//   // Tuple(長さ固定の配列)
+//   role: [2, 'author']
+// };
 
 // pushは許可されてしまう
 // person.role.push('admin')
 
-console.log(person.name);
+// 割与えられる数値は自動でインクリメント（文字列もOK）
+enum Role {
+  ADMIN = 'ADMIN',
+  READ_ONLY = 100,
+  AUTHOR = 200
+}
+
+const person = {
+  name: 'john',
+  age: 30,
+  hobbies: ['Sport', 'Cooking'],
+  role: Role.ADMIN
+};

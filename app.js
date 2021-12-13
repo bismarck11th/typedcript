e41d5ccedb5337550007f8064fab2c5f55bbdccc
kazuki827 <xm.kazuki827@gmail.com>
2021-12-13 22:36:28 +0900

    function type

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 function add(n1, n2) {
     return n1 + n2;
 }
-var number1 = 5;
-var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
+var combineValues;
+combineValues = add;
+// combineValues = printResult; error
+console.log(combineValues(10, 5));
--- app.ts
+++ app.ts
@@ -1,19 +1,23 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text'
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (
-    (typeof input1 === 'number' && typeof input2 === 'number') ||
-    resultConversion === 'as-number'
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
+
+function printResult(num: number): void {
+  console.log('Result: ' + num);
+}
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+  return result;  // No error
+});
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult; error
+console.log(combineValues(10, 5));

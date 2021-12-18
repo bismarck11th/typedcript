6be871f2bf3fb4153c66da1bcbedf1fcd4e73817
kazuki827 <xm.kazuki827@gmail.com>
2021-12-18 22:57:43 +0900

    class - readonly

--- class-interface/app.ts
+++ class-interface/app.ts
@@ -1,7 +1,7 @@
 class Department {
   private employees: string[] = [];
 
-  constructor(private id: string, public name: string) {}
+  constructor(readonly id: string, public name: string) { }
 
   describe(this: Department) {
     console.log(`Department (${this.id}): ${this.name}`);

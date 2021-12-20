b0f43a5a5e16321b543545b940c2722fa10174e5
kazuki827 <xm.kazuki827@gmail.com>
2021-12-21 05:49:25 +0900

    advanced - type guards

--- advanced-type/app.js
+++ advanced-type/app.js
@@ -10,4 +10,46 @@
         privileges: ['create-server'],
         startDate: new Date()
     };
+    function add(a, b) {
+        // プリミティブ型の型ガード
+        if (typeof a === 'string' || typeof b === 'string') {
+            return a.toString() + b.toString();
+        }
+        return a + b;
+    }
+    function printEmployeeInformation(emp) {
+        console.log(emp.name);
+        // object型の型ガード
+        if ('privileges' in emp) {
+            console.log('Privileges: ' + emp.privileges);
+        }
+        if ('startDate' in emp) {
+            console.log('Privileges: ' + emp.startDate);
+        }
+    }
+    class Car {
+        drive() {
+            console.log('運転中...');
+        }
+    }
+    class Truck {
+        drive() {
+            console.log('トラックを運転中...');
+        }
+        loadCargo(amount) {
+            console.log('荷物を乗せています...' + amount);
+        }
+    }
+    const v1 = new Car();
+    const v2 = new Truck();
+    function useVehicle(vehicle) {
+        vehicle.drive();
+        // if ('loadCargo' in vehicle) {
+        //   vehicle.loadCargo(1000)
+        // }
+        // classの型ガード
+        if (vehicle instanceof Truck) {
+            vehicle.loadCargo(1000);
+        }
+    }
 }
--- advanced-type/app.ts
+++ advanced-type/app.ts
@@ -28,7 +28,7 @@
     startDate: Date;
   }
 
-  // interfaceでも同じ実装が可能
+  // interfaceでも同等の実装が可能
   interface ElevatedEmployee2 extends Admin2, Employee2 {}
 
   const e2: ElevatedEmployee1 = {
@@ -42,4 +42,58 @@
 
   // Union型は共通部分が型になる. Object型は結合された型になる.
   type Universal = Combinable & Numeric; // number型
+
+  function add(a: Combinable, b: Combinable) {
+    // プリミティブ型の型ガード
+    if (typeof a === 'string' || typeof b === 'string') {
+      return a.toString() + b.toString();
+    }
+    return a + b;
+  }
+
+  type UnknownEmployee = Employee1 | Admin1;
+
+  function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log(emp.name);
+    // object型の型ガード
+    if ('privileges' in emp) {
+      console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+      console.log('Privileges: ' + emp.startDate);
+    }
+  }
+
+  class Car {
+    drive() {
+      console.log('運転中...');
+    }
+  }
+
+  class Truck {
+    drive() {
+      console.log('トラックを運転中...');
+    }
+
+    loadCargo(amount: number) {
+      console.log('荷物を乗せています...' + amount);
+    }
+  }
+
+  type Vehicle = Car | Truck;
+
+  const v1 = new Car();
+  const v2 = new Truck();
+
+  function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    // if ('loadCargo' in vehicle) {
+    //   vehicle.loadCargo(1000)
+    // }
+
+    // classの型ガード
+    if (vehicle instanceof Truck) {
+      vehicle.loadCargo(1000);
+    }
+  }
 }
